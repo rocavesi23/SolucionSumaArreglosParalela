c9311a63db470d122694753fb4a1bf56aacfd2ad
rasiguen <rasiguen@3531_4974>
2023-01-26 22:04:00 -0600

    Agregar archivos de proyecto.

---
+++ SolucionSumaArreglosParalela.cpp
@@ -0,0 +1,58 @@
+// SolucionSumaArreglosParalela.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// https://stackoverflow.com/questions/11773115/parallel-for-loop-in-openmp 
+
+
+#include <iostream>
+#include <omp.h>
+
+#define N 10000 //Cantidad de elementos a manejar en el arreglo
+#define chunk 1000 //Tamaño que tendrán los pedazos de los arreglos para que cada hilo creado se encargue de esta cantidad de elementos
+#define mostrar 20 //Manejar la cantidad de datos a mostrar
+
+void imprimeArreglo(float* d);
+
+int main()
+{
+    std::cout << "Sumando arreglos en paralelo\n";
+    float a[N], b[N], c[N];
+    int i;
+
+    for (i = 0; i < N; i++) 
+    {
+        a[i] = i * 10;
+        b[i] = (i + 3) * 3.7;
+    }
+
+    int pedazos = chunk;
+
+    #pragma omp parallel for \
+    shared (a, b, c, pedazos) private (i) \
+    schedule (static, pedazos)
+
+    for (i = 0; i < N; i++)
+        c[i] = a[i] + b[i];
+
+    std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo a: " << std::endl;
+    imprimeArreglo(a);
+    std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo b: " << std::endl;
+    imprimeArreglo(b);
+    std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo c: " << std::endl;
+    imprimeArreglo(c);
+}
+
+void imprimeArreglo(float* d)
+{
+    for (int x = 0; x < mostrar; x++)
+        std::cout << d[x] << " - ";
+    std::cout << std::endl;
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
